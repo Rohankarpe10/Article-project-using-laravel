f0385758258c44eff2f2fc7efe44289060a14b1c
Rohankarpe10 <58081468+Rohankarpe10@users.noreply.github.com>
2023-07-16 20:57:42 +0200

    change

--- my-app/my-app/app/Http/Controllers/AuthController.php
+++ my-app/my-app/app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
 
 
         $user->save();
-        // redirect('/login')->with('success', 'Register successfully');
+        //return redirect('/login')->with('success', 'Register successfully');
        return back()->with('success', 'Register successfully');
     }
 
